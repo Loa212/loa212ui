4a016e757f5ca827bb448954dea0080a1145f5a4
Loa212 <anobileloris@gmail.com>
2023-10-12 00:04:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center absolute inset-0">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
+      Hellow
+    </button>
+  );
+};
+
+export { Button };
